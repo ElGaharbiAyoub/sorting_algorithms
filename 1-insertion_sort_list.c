bbe87d1cb744ed52a58df12192713135aca25fc0
elgharbi <elgharbiay@gmail.com>
2023-07-23 00:28:20 +0100

    fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *prev, *next;
 
-	if (!(*list) || (*list)->next == NULL)
+	if (!(*list) || !(*list)->next)
 		return;
 
 	current = *list;
@@ -22,18 +22,26 @@ void insertion_sort_list(listint_t **list)
 		{
 			prev = current->prev;
 			prev->next = current->next;
+
 			if (current->next)
 				current->next->prev = prev;
+
 			current->next = prev;
 			current->prev = prev->prev;
+
 			if (prev->prev)
 				prev->prev->next = current;
+
 			prev->prev = current;
+
 			if (!current->prev)
 				*list = current;
+
 			print_list(*list);
+
 			prev = current->prev;
 		}
 		current = next;
+        current ? (next = current->next) : (next = NULL);
 	}
 }
