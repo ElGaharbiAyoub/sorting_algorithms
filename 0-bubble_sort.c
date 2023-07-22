65a1c0bf1cadf97dd2f1d2e03ffcee8ea634a31d
elgharbi <elgharbiay@gmail.com>
2023-07-22 22:34:28 +0100

    fix some code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,26 +9,25 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0;
-	int valOfInd, isChenged = 0;
+	int valOfInd, isChenged = 1;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (i < size)
+	while (isChenged)
 	{
-		if (array[i] > array[i + 1])
+		isChenged = 0;
+		for (i = 0; i < size - 1; i++)
 		{
-			isChenged = 1;
-			valOfInd = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = valOfInd;
-			print_array(array, size);
-		}
-		i++;
-		if (i == size && isChenged == 1)
-		{
-			i = 0;
-			isChenged = 0;
+			if (array[i] > array[i + 1])
+			{
+				isChenged = 1;
+				valOfInd = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = valOfInd;
+				print_array(array, size);
+			}
 		}
 	}
+
 }
