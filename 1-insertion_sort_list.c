1a74efd2d720526d67adc269c5d0fe0b43d0dbff
elgharbi <elgharbiay@gmail.com>
2023-07-23 00:33:00 +0100

    fix the list is null

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *prev, *next;
 
-	if (!(*list) || !(*list)->next)
+	if (!list || !(*list) || !(*list)->next)
 		return;
 
 	current = *list;
