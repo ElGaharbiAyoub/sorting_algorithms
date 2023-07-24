e977ce186efe3e0d74078d8aa7c5debc7792947f
elgharbi <elgharbiay@gmail.com>
2023-07-25 00:45:07 +0100

    Shell sort - Knuth Sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sorts an array of integers in ascending order using Shell sort
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, j;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	/*Generate the Knuth sequence for intervals*/
+	while (gap < size / 3)
+	{
+		gap = (gap * 3) + 1;
+	}
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
+			j = i;
+			while (j >= gap && array[j - gap] > tmp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = tmp;
+			print_array(array, size);
+		}
+		/*Reduce the gap as per Knuth sequence*/
+		gap = (gap - 1) / 3;
+
+	}
+}
